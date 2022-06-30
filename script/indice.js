bd955a65c873863266857bf8d3c405013103a7ba
danielontivero <danielontivero@hotmail.com>
2022-06-30 13:55:42 -0300

    entrega complementaria306

--- script/indice.js
+++ script/indice.js
@@ -1,12 +1,12 @@
 window.onload = iniciar;
 // DOM
-let nombre = prompt("Por Favor Ingrese sus Datos")
+/* let nombre = prompt("Por Favor Ingrese sus Datos")
 
 const bienvenida = document.getElementById("saludo")
 const saludo = document.createElement('h1')
 saludo.textContent = "Hola " + nombre 
 bienvenida.append(saludo)
-let detalleDeCompra  = document.getElementById("detalleDeCompra")
+let detalleDeCompra  = document.getElementById("detalleDeCompra") */
 
 const contenedor = document.getElementById("contenedor")
 const titulo = document.createElement('h1')
@@ -16,7 +16,7 @@ contenedor.append(titulo)
 let listadoDeProductos = document.getElementById("listadoDeProductos")
 
 let carrito = []
-console.log(carrito)
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 function mostrarProductos() {
     products.forEach((product) => {
@@ -32,12 +32,17 @@ function mostrarProductos() {
         let buyButton = document.createElement("button")
         buyButton.innerText = ("Agregar al carrito")
         card.append(img, name, price, buyButton)
-
+        carritoVacio()
         buyButton.addEventListener("click", function () {
             carrito.push(product)
             alert("Agregaste " + product.name + " al carrito")
             detalleDeCompra.innerHTML = `` 
             mostrarCarrito()
+            // ejemplo de desestructurarion de array
+            const [a] = carrito
+            console.log(a) 
+
+            
         }) 
     }) 
 } 
@@ -57,48 +62,39 @@ function mostrarCarrito() {
             carrito = []
             detalleDeCompra.innerHTML =``
             sumaTotal()
+            carritoVacio() 
         })
-    //Local Storage 
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    let storage = localStorage.getItem("carrito")
-    console.log(JSON.parse(storage))
+        //Local Storage 
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        let storage = localStorage.getItem("carrito")
+        console.log(JSON.parse(storage))
     })
-    }
-
-//CARRITO VACIO: 
-if (!carrito.length) {
-    detalleDeCompra.innerHTML = ("El carrito esta vacio")
-}  
-function sumaTotal (){
-    const importes = carrito.map(({price})=>price) 
-    console.log(importes) 
-    
-    const acumular = (acumulador, importes) => acumulador + importes ;
-    let total = importes.reduce(acumular, 0)
-    return total;
-    
 }
-// llamado a las funciones
+//CARRITO VACIO: 
+const carritoVacio = () =>  carrito.length === 0 && (detalleDeCompra.innerHTML = ("El carrito esta vacio"))
+//const carritoVacio = carrito == 0 ? detalleDeCompra.innerHTML = ("El carrito esta vacio") : ""
+//if (!carrito.length) {detalleDeCompra.innerHTML = ("El carrito esta vacio")} 
+
+function sumaTotal (){
+const total = carrito.map((item) => parseInt(item.price)).reduce((carritoTotalPrice, currentItemPrice) => carritoTotalPrice + currentItemPrice, 0);
+return total;
+    // const importes = carrito.map(({price})=>price) 
+    // console.log(importes) 
+    
+    // const acumular = (acumulador, importes) => acumulador + importes ;
+    // let total = importes.reduce(acumular, 0)
+    // return total; 
+}
+//llamado a las funciones
 mostrarProductos()
 iniciar()
-
 // boton
 function iniciar(){
     let btnCalcular = document.getElementById("btnCalcular");
     btnCalcular.addEventListener("click", clickBtnCalcular);    
 }   
-
 function clickBtnCalcular() {
     let btnCalcular = document.getElementById("btnCalcular")
     btnCalcular = alert("el total es $ " + sumaTotal())
 }  
-
-////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
 
